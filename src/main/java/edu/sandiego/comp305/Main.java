dfea2925e21ef5dfc2e4d17652882eba64433c6c
Travis <twelfle@sandiego.edu>
2026-06-29 16:05:47 -0700

    update name

--- src/main/java/edu/sandiego/comp305/Main.java
+++ src/main/java/edu/sandiego/comp305/Main.java
@@ -6,5 +6,6 @@ public class Main {
         System.out.println("Hello Joe Roni");
         System.out.println("Hello, Dr. Krause-Levy!");
         System.out.println(" Hello, Devon Montgomery-Eder!");
+        System.out.println("Hello, Travis Welfle");
     }
 }
